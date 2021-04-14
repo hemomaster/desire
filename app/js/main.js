aac4667b91d0173985d903d4557a27369e1ff06e
hemomaster <hemomaster@gmail.com>
2021-04-14 20:20:03 +0300

    Сверстал, стилизовал боковое меню.Подключил скрипт.

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("ok");
+  const sideMenu = document.querySelector(".side-menu");
+
+  // добавить/удалить класс активности "active" у элемента ".side-menu"
+  const toggleSideMenu = () => sideMenu.classList.toggle("active");
+
+  sideMenu.addEventListener("click", (e) => {
+    const target = e.target;
+    if (
+      (target === sideMenu && !target.closest(".side-menu__body")) ||
+      target.closest(".side-menu__close")
+    )
+      toggleSideMenu();
+  });
+
+  document.querySelector(".header__btn").addEventListener("click", () => {
+    toggleSideMenu();
+  });
 });
