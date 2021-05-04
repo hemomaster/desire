b75245bfeb9a315255b59af7c8f45741caecc178
hemomaster <hemomaster@gmail.com>
2021-05-04 15:13:02 +0300

    Адаптировал Post Page

--- gulpfile.js
+++ gulpfile.js
@@ -21,18 +21,20 @@ function cleanDist() {
 }
 
 function images() {
-  return src("app/images/**/*")
-    .pipe(
-      imagemin([
-        imagemin.gifsicle({ interlaced: true }),
-        imagemin.mozjpeg({ quality: 75, progressive: true }),
-        imagemin.optipng({ optimizationLevel: 5 }),
-        imagemin.svgo({
-          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
-        }),
-      ])
-    )
-    .pipe(dest("dist/images"));
+  return (
+    src("app/images/**/*")
+      // .pipe(
+      //   imagemin([
+      //     imagemin.gifsicle({ interlaced: true }),
+      //     imagemin.mozjpeg({ quality: 75, progressive: true }),
+      //     imagemin.optipng({ optimizationLevel: 5 }),
+      //     imagemin.svgo({
+      //       plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+      //     }),
+      //   ])
+      // )
+      .pipe(dest("dist/images"))
+  );
 }
 
 function scripts() {
